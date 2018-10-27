867050b0fa6bf3b3b403b6802d6961f660b27a98
Ruben Mechelinck <ruben.mechelinck@hotmail.be>
2018-10-27 15:22:49 +0200

    Initial Commit

--- src/be/kul/gantry/domain/Main.java
+++ src/be/kul/gantry/domain/Main.java
@@ -5,7 +5,7 @@ import java.io.File;
 import java.io.FileWriter;
 
 public class Main {
-    public static void main(String [ ] args){
+    public static void main(String[] args){
         try{
             Problem problem = Problem.fromJson(new File("1_10_100_4_FALSE_65_50_50.json"));
 
